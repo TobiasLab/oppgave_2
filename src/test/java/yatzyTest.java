8608beb3e35eed4161d0ec02b406eacbc4788824
TobiasLab <tobias.labidi@gmail.com>
2019-09-01 22:47:30 +0200

    some code

--- src/test/java/yatzyTest.java
+++ src/test/java/yatzyTest.java
@@ -1,5 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class yatzyTest {
     @Test
+    void shouldCalculateForOnes() {
+        assertEquals(0, score("ONES", new int[] { 2, 3, 4, 5, 6 }));
+        assertEquals(3, score("ONES", new int[] { 2, 1, 4, 1, 1 }));
+        assertEquals(5, score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+    }
+
+    private Object score(String ones, int[] ints) {
+        return 
+    }
 }
