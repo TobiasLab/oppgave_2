b27c14aaf28a44b97ae40bbe243a2a4b4a982ee4
TobiasLab <tobias.labidi@gmail.com>
2019-09-05 23:44:22 +0200

    started of threes test

--- src/test/java/yatzyTest.java
+++ src/test/java/yatzyTest.java
@@ -39,9 +39,9 @@ public class yatzyTest {
 
     @Test
     void shouldCalculateForThrees() {
-        assertEquals( 0, scoreThrees("THREES", new int[] { 1, 3, 4, 5, 6}));
-        assertEquals( 3, scoreThrees("THREES", new int[] { 1, 3, 4, 5, 6}));
-        assertEquals( 5, scoreThrees("THREES", new int[] { 1, 3, 4, 5, 6}));
+        assertEquals( 0, scoreThrees("THREES", new int[] { 1, 2, 4, 5, 6}));
+        assertEquals( 3, scoreThrees("THREES", new int[] { 3, 3, 3, 5, 6}));
+        assertEquals( 5, scoreThrees("THREES", new int[] { 3, 3, 3, 3, 3}));
     }
 
     private int scoreThrees(String category, int[] dice) {
